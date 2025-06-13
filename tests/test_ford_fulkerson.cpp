8d6a15c9e37834b40d0743bbe9cac3127ed281a1
Fernando Rocha <A00836634@tec.mx>
2025-06-13 15:51:11 -0600

    Correccion de test ford fulkerson

---
+++ tests/test_ford_fulkerson.cpp
@@ -0,0 +1,56 @@
+#include <gtest/gtest.h>
+#include "../ford_fulkerson.h"
+
+class FordFulkersonTest : public ::testing::Test
+{
+protected:
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+TEST_F(FordFulkersonTest, BasicFlow)
+{
+    std::vector<std::vector<int>> capacity = {
+        {0, 16, 13, 0, 0, 0},
+        {0, 0, 10, 12, 0, 0},
+        {0, 4, 0, 0, 14, 0},
+        {0, 0, 9, 0, 0, 20},
+        {0, 0, 0, 7, 0, 4},
+        {0, 0, 0, 0, 0, 0}};
+
+    int maxFlow = fordFulkerson(capacity, 0, 5);
+    EXPECT_EQ(maxFlow, 23);
+}
+
+TEST_F(FordFulkersonTest, SimpleFlow)
+{
+    std::vector<std::vector<int>> capacity = {
+        {0, 10, 10},
+        {0, 0, 25},
+        {0, 0, 0}};
+
+    int maxFlow = fordFulkerson(capacity, 0, 2);
+    EXPECT_EQ(maxFlow, 10);
+}
+
+TEST_F(FordFulkersonTest, NoFlow)
+{
+    std::vector<std::vector<int>> capacity = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}};
+
+    int maxFlow = fordFulkerson(capacity, 0, 2);
+    EXPECT_EQ(maxFlow, 0);
+}
+
+TEST_F(FordFulkersonTest, SinglePath)
+{
+    std::vector<std::vector<int>> capacity = {
+        {0, 5, 0},
+        {0, 0, 3},
+        {0, 0, 0}};
+
+    int maxFlow = fordFulkerson(capacity, 0, 2);
+    EXPECT_EQ(maxFlow, 3);
+}
