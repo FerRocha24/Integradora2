25ca2faa90fa2cac837473691edcd41ab4ac31a2
dggalvans <dggalvans@gmail.com>
2025-06-13 17:34:12 -0600

    cambio header

--- ford_fulkerson.cpp
+++ ford_fulkerson.cpp
@@ -1,3 +1,23 @@
+/*
+ * SonarQube, open source software quality management tool.
+ * Copyright (C) 2008-2013 SonarSource
+ * http://github.com/SonarOpenCommunity/sonar-cxx
+ *
+ * SonarQube is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * SonarQube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include "ford_fulkerson.h"
 
 bool bfs(std::vector<std::vector<int>> &rGraph, int s, int t, std::vector<int> &parent)
@@ -63,3 +83,4 @@ int fordFulkerson(std::vector<std::vector<int>> capacity, int s, int t)
 
     return maxFlowValue;
 }
+
